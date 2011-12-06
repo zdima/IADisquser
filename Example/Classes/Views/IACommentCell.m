0d3220ecf17a8412d512b19dfe7e588854b7d2db
Ikhsan Assaat <ixnixnixn@yahoo.com>
2011-12-06 23:21:52 +0700

    tableview's cells dynamic height fixed

--- Example/Classes/Views/IACommentCell.m
+++ Example/Classes/Views/IACommentCell.m
@@ -70,7 +70,6 @@
     [imageView setFrame:CGRectMake(4.0, 8.0, 44.0, 44.0)];
     [nameLabel setFrame:CGRectMake(60.0, 8.0, 200.0, 20.0)];
     [dateLabel setFrame:CGRectMake(60.0, 32.0, 200.0, 15.0)];
-    //[commentLabel setFrame:CGRectMake(4.0, 65.0, 300.0, 60.0)];
 }
 
 - (void)drawRect:(CGRect)rect {
